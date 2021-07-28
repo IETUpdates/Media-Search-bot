deae990fb6616d92ba4964bd4ddb1dd2c92b8f89
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-07-29 00:31:10 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -84,7 +84,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('More Movies', url='https://t.me/mm_moviess')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
