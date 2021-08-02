b27785737ea3d65e766bff4fee422bb88fb32fb3
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-08-02 21:35:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(bot, cmd):
                     f_caption = f"@MM_MOVIESS {files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
+                        InlineKeyboardButton('♣️ Updates', url='https://t.me/iet_updates'),
                         InlineKeyboardButton('👩‍💻 Dev', url='https://t.me/iet_owner')
                     ]
                     ]
@@ -99,7 +99,7 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("♣️ Updates", url='https://t.me/iet_updates"),
                         InlineKeyboardButton("👩‍💻 Dev", url="https://t.me/iet_owner")
                     ],
                     [
